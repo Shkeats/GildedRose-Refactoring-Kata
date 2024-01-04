e9f7503e34e41e3adb9aa8270851a9c6dbbc3070
Shaun Keating <shaun@blackfrog.co>
2024-01-04 15:27:42 +0000

    Add a failing test case for the new conjured items requirement. Interpreted as any item containing the word "Conjured".

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -194,4 +194,23 @@ class GildedRoseTest extends TestCase
         $this->assertSame(-2, $items[0]->sellIn);
         $this->assertSame(0, $items[0]->quality);
     }
+
+    public function test_that_conjured_items_degrade_in_quality_at_2_per_day()
+    {
+        $items = [
+            new Item('Conjured Potato', 5, 10),
+            new Item('Gold Ring (Conjured)', 5, 50),
+        ];
+
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertSame(8, $items[0]->quality);
+        $this->assertSame(48, $items[1]->quality);
+        $gildedRose->updateQuality();
+        $this->assertSame(6, $items[0]->quality);
+        $this->assertSame(46, $items[1]->quality);
+        $gildedRose->updateQuality();
+        $this->assertSame(4, $items[0]->quality);
+        $this->assertSame(44, $items[1]->quality);
+    }
 }
